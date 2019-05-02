489db4d35e88cce559298247af567c7c481133a7
Brian Botha <tegefaulkes@gmail.com>
2019-05-03 01:28:57 +1000

    added test benches for messageScheduler and compress. Both test OK

--- src/main/scala/sha256/SHA256.scala
+++ src/main/scala/sha256/SHA256.scala
@@ -1,31 +1,124 @@
 package sha256
 
 import chisel3._
+import chisel3.util._
 
 
 class messageScheduler extends Module{
   val io = IO(new Bundle {
     val messageIn = Input(UInt(512.W))
     val load      = Input(Bool())
-    val en        = Input(Bool())
+//    val en        = Input(Bool())
     val W         = Output(UInt(32.W))
   })
 
   val registers = Reg(Vec(16, UInt(32.W)))
-  val sig0  = (W1(6,0) ## W1(31,7)) ^ (W1(17,0) ## W1(31,18)) ^ (W1 >> 3).asUInt()
-  val sig1  = (W2(16,0) ## W2(31,17)) ^ (W2(18,0) ## W2(31,19)) ^ (W2 >> 10).asUInt()
   val W1     = registers(1)
   val W2     = registers(14)
+  val sig0  = (W1(6,0) ## W1(31,7)) ^ (W1(17,0) ## W1(31,18)) ^ (W1 >> 3).asUInt()
+  val sig1  = (W2(16,0) ## W2(31,17)) ^ (W2(18,0) ## W2(31,19)) ^ (W2 >> 10).asUInt()
+
+  io.W := registers(0)
 
   when (io.load){
     for (i <- 0 to 15){
-      registers(i) :=  io.messageIn((i*32)+31, i*32)
+      registers(i) :=  io.messageIn(((15-i)*32)+31, (15-i)*32)
     }
-  }.elsewhen(io.en) {
-    io.W := registers(0)
+  }.otherwise {
     for(i <- 1 to 15){
       registers(i-1) := registers(i)
     }
     registers(15) := sig0 +% registers(9) +% sig1 +% registers(0)
+  }
+}
+
+class compressionFunction(debugTap: Boolean) extends Module{
+  val io = IO(new Bundle{
+    val messageBlock    = Input(UInt(512.W))
+    val start           = Input(Bool())
+//    val initialState    = Input(Vec(8, UInt(32.W)))
+    val registerOut     = Output(Vec(8, UInt(32.W)))
+    val ready           = Output(Bool())
+    val stateTap        = if (debugTap) Some(Output(Vec(8, UInt(32.W)))) else None
+    val loopCountTap    = if (debugTap) Some(Output(UInt(32.W))) else None
+  })
+
+  val scheduler = Module(new messageScheduler)
+
+  val K = VecInit(Array("h_428a2f98".U, "h_71374491".U, "h_b5c0fbcf".U, "h_e9b5dba5".U, "h_3956c25b".U, "h_59f111f1".U, "h_923f82a4".U, "h_ab1c5ed5".U,
+                        "h_d807aa98".U, "h_12835b01".U, "h_243185be".U, "h_550c7dc3".U, "h_72be5d74".U, "h_80deb1fe".U, "h_9bdc06a7".U, "h_c19bf174".U,
+                        "h_e49b69c1".U, "h_efbe4786".U, "h_0fc19dc6".U, "h_240ca1cc".U, "h_2de92c6f".U, "h_4a7484aa".U, "h_5cb0a9dc".U, "h_76f988da".U,
+                        "h_983e5152".U, "h_a831c66d".U, "h_b00327c8".U, "h_bf597fc7".U, "h_c6e00bf3".U, "h_d5a79147".U, "h_06ca6351".U, "h_14292967".U,
+                        "h_27b70a85".U, "h_2e1b2138".U, "h_4d2c6dfc".U, "h_53380d13".U, "h_650a7354".U, "h_766a0abb".U, "h_81c2c92e".U, "h_92722c85".U,
+                        "h_a2bfe8a1".U, "h_a81a664b".U, "h_c24b8b70".U, "h_c76c51a3".U, "h_d192e819".U, "h_d6990624".U, "h_f40e3585".U, "h_106aa070".U,
+                        "h_19a4c116".U, "h_1e376c08".U, "h_2748774c".U, "h_34b0bcb5".U, "h_391c0cb3".U, "h_4ed8aa4a".U, "h_5b9cca4f".U, "h_682e6ff3".U,
+                        "h_748f82ee".U, "h_78a5636f".U, "h_84c87814".U, "h_8cc70208".U, "h_90befffa".U, "h_a4506ceb".U, "h_bef9a3f7".U, "h_c67178f2".U))
+
+  val sReady :: sLoop :: Nil = Enum(2)
+  val state = RegInit(sReady)
+  val registers = Reg(Vec(8, UInt(32.W)))
+  val loopCount = RegInit(0.U(7.W))
+
+  val W1 = registers(4)
+  val W2 = registers(0)
+  val SIG1 = Cat(W1(5,0) , W1(31,6)) ^ Cat(W1(10,0) , W1(31,11)) ^ Cat(W1(24,0) , W1(31,25))
+  val SIG0 = Cat(W2(1,0) , W2(31,2)) ^ Cat(W2(12,0) , W2(31,13)) ^ Cat(W2(21,0) , W2(31,22))
+  val Ch = (registers(4) & registers(5)) ^ ((registers(4) ^ "h_FFFFFFFF".U) & registers(6))
+  val Maj = (registers(0) & registers(1)) ^ (registers(0) & registers(2)) ^ (registers(1) & registers(2))
+
+  val T1 = registers(7) +% SIG1 +% Ch +% K(loopCount) +% scheduler.io.W
+  val T2 = SIG0 +% Maj
+
+  scheduler.io.messageIn := io.messageBlock
+  io.registerOut := VecInit(Array(0.U,0.U,0.U,0.U,0.U,0.U,0.U,0.U))
+  if(debugTap) {
+    io.stateTap.get := registers
+    io.loopCountTap.get := loopCount
+  }
+
+  scheduler.io.load := true.B
+  io.ready := true.B
+
+
+  switch (state){
+    is (sReady){
+      loopCount := 0.U
+      scheduler.io.load := true.B
+      io.ready := true.B
+
+
+      when(io.start){
+        state := sLoop
+        registers(0) := "h_6a09e667".U(32.W)  //FIXME, have the correct output WHILE ready = true.
+        registers(1) := "h_bb67ae85".U(32.W)
+        registers(2) := "h_3c6ef372".U(32.W)
+        registers(3) := "h_a54ff53a".U(32.W)
+        registers(4) := "h_510e527f".U(32.W)
+        registers(5) := "h_9b05688c".U(32.W)
+        registers(6) := "h_1f83d9ab".U(32.W)
+        registers(7) := "h_5be0cd19".U(32.W)
+
+        //registers := io.initialState
+      }
+    }
+    is (sLoop){
+      scheduler.io.load := false.B
+      io.ready := false.B
+      when(loopCount < 64.U) {
+        registers(7) := registers(6)
+        registers(6) := registers(5)
+        registers(5) := registers(4)
+        registers(4) := registers(3) +% T1
+        registers(3) := registers(2)
+        registers(2) := registers(1)
+        registers(1) := registers(0)
+        registers(0) := T1 +% T2
+        loopCount := loopCount +% 1.U
+      }.otherwise{
+        io.ready := true.B
+        io.registerOut := registers
+        state := sReady
+      }
+    }
   }
 }
--- src/test/scala/sha256/SHA256Test.scala
+++ src/test/scala/sha256/SHA256Test.scala
@@ -1,7 +1,10 @@
 // See README.md for license details.
 package sha256
 
-object SHA256Test {
+import chisel3.iotesters._
+import chisel3._
+
+class SHA256Test {
 
   var K = Array("428a2f98", "71374491", "b5c0fbcf", "e9b5dba5", "3956c25b", "59f111f1", "923f82a4", "ab1c5ed5",
                 "d807aa98", "12835b01", "243185be", "550c7dc3", "72be5d74", "80deb1fe", "9bdc06a7", "c19bf174",
@@ -12,20 +15,20 @@ object SHA256Test {
                 "19a4c116", "1e376c08", "2748774c", "34b0bcb5", "391c0cb3", "4ed8aa4a", "5b9cca4f", "682e6ff3",
                 "748f82ee", "78a5636f", "84c87814", "8cc70208", "90befffa", "a4506ceb", "bef9a3f7", "c67178f2")
 
-  def main(args: Array[String]): Unit = {
-    //println(preprocess(BigInt("616263",16)).toString(16))
-    //println(preprocess(BigInt("6162636462636465636465666465666765666768666768696768696a68696a6b696a6b6c6a6b6c6d6b6c6d6e6c6d6e6f6d6e6f706e6f7071", 16)).toString(16))
-
+//  def main(args: Array[String]): Unit = {
+//    //println(preprocess(BigInt("616263",16)).toString(16))
+//    //println(preprocess(BigInt("6162636462636465636465666465666765666768666768696768696a68696a6b696a6b6c6a6b6c6d6b6c6d6e6c6d6e6f6d6e6f706e6f7071", 16)).toString(16))
+//
 //    val processed = preprocess(BigInt("616263", 16))
-    //val processed = preprocess(BigInt("6162636462636465636465666465666765666768666768696768696a68696a6b696a6b6c6a6b6c6d6b6c6d6e6c6d6e6f6d6e6f706e6f7071", 16))
-    val processed = preprocess(BigInt("54686520517569636b2042726f776e20466f78204a756d706564204f76657220546865204c617a7920446f67",16))
-    //println(processed.toString(32))
-    //println(processed.toString(16))
-    //println(messageScheduel(processed).length)
-    println(hash(processed).toString(16))
-    //println(getSubChunk(BigInt("616263646263646563646566", 16), 32, 2, 3).toString(16))
-
-  }
+//    //val processed = preprocess(BigInt("6162636462636465636465666465666765666768666768696768696a68696a6b696a6b6c6a6b6c6d6b6c6d6e6c6d6e6f6d6e6f706e6f7071", 16))
+//    //val processed = preprocess(BigInt("54686520517569636b2042726f776e20466f78204a756d706564204f76657220546865204c617a7920446f67",16))
+//    //println(processed.toString(32))
+//    //println(processed.toString(16))
+//    //println(messageScheduel(processed).length)
+//    println(hash(processed).toString(16))
+//    //println(getSubChunk(BigInt("616263646263646563646566", 16), 32, 2, 3).toString(16))
+//
+//  }
 
   def getSubBits(in: BigInt, a: Int, b: Int): BigInt = { //WORKING
     val width = a - b + 1
@@ -40,7 +43,7 @@ object SHA256Test {
     (in & (mask << shiftPosition)) >> shiftPosition
   }
 
-  def messageScheduel(mess:BigInt):Array[BigInt] = {
+  def messageSchedule(mess:BigInt):Array[BigInt] = {
 
     def sig0(x:BigInt):BigInt = {
       rotR32(x, 7) ^ rotR32(x, 18) ^ (x >> 3)
@@ -71,24 +74,52 @@ object SHA256Test {
     ((number >> bits) + (chunk << (32 - bits))) & (BigInt(2).pow(32)-1)
   }
 
+
+  def SIG0(x:BigInt):BigInt = {
+    rotR32(x, 2) ^ rotR32(x, 13) ^ rotR32(x, 22)
+  }
+
+  def SIG1(x:BigInt):BigInt = {
+    rotR32(x, 6) ^ rotR32(x, 11) ^ rotR32(x, 25)
+  }
+
+  def Ch(x:BigInt, y:BigInt, z:BigInt):BigInt = {
+    ((x & y) ^ (~x & z)) & BigInt(2).pow(32)-1
+  }
+
+
+  def Maj(x:BigInt, y:BigInt, z:BigInt):BigInt = {
+    (x & y) ^ (x & z) ^ (y & z)
+  }
+
+  def compress(in1: BigInt,in2: BigInt,in3: BigInt,in4: BigInt,in5: BigInt,in6: BigInt,in7: BigInt,in8: BigInt, W: Array[BigInt]):Array[BigInt] = {
+    var a = in1
+    var b = in2
+    var c = in3
+    var d = in4
+    var e = in5
+    var f = in6
+    var g = in7
+    var h = in8
+
+    for (j <- 0 to 63) {
+      val T1 = (h + SIG1(e) + Ch(e, f, g) + BigInt(K(j), 16) + W(j)) & (BigInt(2).pow(32) - 1)
+      val T2 = (SIG0(a) + Maj(a, b, c)) & (BigInt(2).pow(32) - 1)
+      h = g
+      g = f
+      f = e
+      e = (d + T1) & (BigInt(2).pow(32) - 1)
+      d = c
+      c = b
+      b = a
+      a = (T1 + T2) & (BigInt(2).pow(32) - 1)
+
+    }
+    Array(a,b,c,d,e,f,g,h)
+  }
+
   def hash(mess:BigInt):BigInt = {
 
-    def SIG0(x:BigInt):BigInt = {
-      rotR32(x, 2) ^ rotR32(x, 13) ^ rotR32(x, 22)
-    }
-
-    def SIG1(x:BigInt):BigInt = {
-      rotR32(x, 6) ^ rotR32(x, 11) ^ rotR32(x, 25)
-    }
-
-    def Ch(x:BigInt, y:BigInt, z:BigInt):BigInt = {
-      ((x & y) ^ (~x & z)) & BigInt(2).pow(32)-1
-    }
-
-
-    def Maj(x:BigInt, y:BigInt, z:BigInt):BigInt = {
-      (x & y) ^ (x & z) ^ (y & z)
-    }
 
     val mLength = mess.toString(16).length()/128
 
@@ -110,41 +141,21 @@ object SHA256Test {
     var g = BigInt(0)
     var h = BigInt(0)
 
+
     for(subChunk <- 0 until mLength){
-      val W = messageScheduel(getSubChunk(mess, 512, subChunk, mLength))
+      val W = messageSchedule(getSubChunk(mess, 512, subChunk, mLength))
 //      BigInt(mess.toString(16).substring(i*128, (i*128)+128), 16)
 
-      a = H1
-      b = H2
-      c = H3
-      d = H4
-      e = H5
-      f = H6
-      g = H7
-      h = H8
+      val alp = compress(H1, H2, H3, H4, H5, H6, H7, H8, W)
 
-      for(j <- 0 to 63){
-        val T1 = ( h + SIG1(e) + Ch(e, f, g) + BigInt(K(j), 16) + W(j)) & (BigInt(2).pow(32)-1)
-        val T2 = ( SIG0(a) + Maj(a, b, c)) & (BigInt(2).pow(32)-1)
-        h = g
-        g = f
-        f = e
-        e = (d + T1) & (BigInt(2).pow(32)-1)
-        d = c
-        c = b
-        b = a
-        a = (T1 + T2) & (BigInt(2).pow(32)-1)
-
-      }
-
-      H1 = (a + H1) & (BigInt(2).pow(32)-1)
-      H2 = (b + H2) & (BigInt(2).pow(32)-1)
-      H3 = (c + H3) & (BigInt(2).pow(32)-1)
-      H4 = (d + H4) & (BigInt(2).pow(32)-1)
-      H5 = (e + H5) & (BigInt(2).pow(32)-1)
-      H6 = (f + H6) & (BigInt(2).pow(32)-1)
-      H7 = (g + H7) & (BigInt(2).pow(32)-1)
-      H8 = (h + H8) & (BigInt(2).pow(32)-1)
+      H1 = (alp(0) + H1) & (BigInt(2).pow(32)-1)
+      H2 = (alp(1) + H2) & (BigInt(2).pow(32)-1)
+      H3 = (alp(2) + H3) & (BigInt(2).pow(32)-1)
+      H4 = (alp(3) + H4) & (BigInt(2).pow(32)-1)
+      H5 = (alp(4) + H5) & (BigInt(2).pow(32)-1)
+      H6 = (alp(5) + H6) & (BigInt(2).pow(32)-1)
+      H7 = (alp(6) + H7) & (BigInt(2).pow(32)-1)
+      H8 = (alp(7) + H8) & (BigInt(2).pow(32)-1)
 
     }
 
@@ -156,3 +167,73 @@ object SHA256Test {
 
 
 }
+
+class messageSchedulerTester(dut: messageScheduler) extends PeekPokeTester(dut){
+  val sha = new SHA256Test
+  val processed = sha.preprocess(BigInt("616263", 16))
+  val schedule = sha.messageSchedule(processed)
+  //TODO write the rest of this
+  //peekpoke shite
+  poke(dut.io.load, true.B)
+  poke(dut.io.messageIn, processed.U)
+  step(1)
+  poke(dut.io.load, false.B)
+  for(i <- 0 to 63) {
+    val result = peek(dut.io.W)
+    val expected = schedule(i)
+    //println("Result: " + result + " expected: " + expected + " is: " + (result == expected))
+    expect(result.U, expected.U)
+    step(1)
+
+  }
+}
+
+class compressionFunctionTester(dut: compressionFunction) extends PeekPokeTester(dut){
+  val sha = new SHA256Test
+  val processed = sha.preprocess(BigInt("616263", 16))
+  val W = sha.messageSchedule(processed)
+  val expectedResult = sha.compress(BigInt("6a09e667",16) ,BigInt("bb67ae85",16) ,BigInt("3c6ef372",16) ,BigInt("a54ff53a",16),BigInt("510e527f",16) ,BigInt("9b05688c",16) ,BigInt("1f83d9ab",16),BigInt("5be0cd19",16), W)
+
+  //poke(dut.io.initialState, VecInit(Array("h_6a09e667".U, "h_bb67ae85".U, "h_3c6ef372".U, "h_a54ff53a".U, "h_510e527f".U, "h_9b05688c".U, "h_1f83d9ab".U, "h_5be0cd19".U)))
+  poke(dut.io.messageBlock, processed.U)
+  poke(dut.io.start, true.B)
+  step(1)
+  poke(dut.io.start, false.B)
+  for(i <- 0 to 63) {
+//    var ready = peek(dut.io.ready)
+//    val result = peek(dut.io.registerOut)
+//    print("result: ")
+//    for (i <- result) {
+//      print(i.toString(16) + " ")
+//    }
+//    println()
+//    println("ready: " + ready.toString())
+//    var loopCount = peek(dut.io.loopCountTap.get)
+//    println("LoopCount: " + loopCount)
+//    println("------------------------------")
+    step(1)
+  }
+  val result = peek(dut.io.registerOut)
+  for(i <- 0 to 7){
+    println("result:  "+ result(i).toString(16) + " Expected: " + expectedResult(i).toString(16) + " is:  " + (result(i) == expectedResult(i)))
+    expect(result(i).U, expectedResult(i).U)
+  }
+  println()
+
+}
+
+object testMessageScheduler extends App {
+  chisel3.iotesters.Driver(() => new messageScheduler()) {
+    c => new messageSchedulerTester(c)
+  }
+
+  chisel3.iotesters.Driver(() => new compressionFunction(true)){
+    c => new compressionFunctionTester(c)
+  }
+}
+
+
+
+//TODO write the rest of the tests
+
+
